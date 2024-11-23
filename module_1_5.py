a9af143aa787498a34e71c0e419614595cff4196
nyuri8691 <168839548+nyuri8691@users.noreply.github.com>
2024-11-23 19:48:41 +0300

    Add files via upload
    Практическое задание по работе в Pycharm - "Переменные". module_1_5

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+kol_vyp_dz = 12
+kol_zat_hour = 1.5
+name_kurs = 'Python'
+zat_hour = kol_zat_hour / kol_vyp_dz
+print('Курс:', name_kurs, 'всего задач:',kol_vyp_dz, 'затрачено часов', kol_zat_hour, 'среднее время выполнения',zat_hour,'часа.')
