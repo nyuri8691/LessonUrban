23d965e0c27dddcd25521f4ec59cc200cda16101
nyuri8691 <168839548+nyuri8691@users.noreply.github.com>
2024-11-28 14:44:48 +0300

    Add files via upload
    Практическое задание по теме: "Словари и множества"

---
+++ module_1_6_.py
@@ -0,0 +1,11 @@
+my_dict = {'Yuri': 1968}
+print(my_dict)
+print(my_dict['Yuri'])
+print(my_dict.get('Egor'))
+my_dict.update({'Egor': 1999,'Masha': 2002})
+my_dict.pop('Masha')
+print(my_dict)
+mi_set = {1, 'apple',42.314, 1, 42.314}
+print(mi_set)
+mi_set.add((5,6,1.6))
+print(mi_set)
